ef3c3402d189219679ef9a0a146830aeb0b92fc6
Greg Jansen <gregory.jansen@gmail.com>
2019-04-10 10:40:05 -0400

    better test output and README

--- tests/test_punchcards.py
+++ tests/test_punchcards.py
@@ -2,9 +2,18 @@
 def test_examples():
     from PIL import Image
     from punchcards import read_card
+    files = [
+        'images/C04D01L-0001.tif',
+        'images/C04D01L-0001-90.tif',
+        'images/C04D01L-0001-big-canvas.png',
+        'images/C04D01L-0001-cropped-tight.tif',
+        'images/sample.png',
+    ]
     text = 'MACON FORT                       4628      NORTH CAROLINA                        '
-    assert read_card('images/C04D01L-0001.tif') == text
-    assert read_card('images/C04D01L-0001-90.tif') == text
-    assert read_card('images/C04D01L-0001-big-canvas.png') == text
-    assert read_card('images/C04D01L-0001-cropped-tight.tif') == text
-    assert read_card('images/sample.png') == text
+    for file in files:
+        result = read_card(file)
+        if not(result == text):
+            print("Read errors for {0}".format(file))
+            image = Image.open(file)
+            image.show()
+        assert result == text
