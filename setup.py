724d502e8c7ac0e2ed2ca497e281214150c04f10
Gregory N Jansen <gregory.jansen@gmail.com>
2019-05-09 15:18:07 -0400

    fixed license

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name="punchcards",
       url='http://github.com/UMD-DCIC/punchcards',
       author='Gregory N. Jansen',
       author_email='jansen@umd.edu',
-      url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
       download_url = 'https://github.com/UMD-DCIC/punchcards/archive/v_01.tar.gz',
       keywords = ['PUNCH', 'CARD', 'PUNCHCARD', 'IMAGE'],
       license='GPL 3.0',
@@ -16,7 +15,7 @@ setup(name="punchcards",
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GPL 3.0',   # Again, pick a license
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
         'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
